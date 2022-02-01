cefea91c36bb7156df56cf51cfc006b08ac300b4
manshur39 <muhammadtm35@gmail.com>
2022-02-01 07:23:18 +0700

    menambah rumus persegi panjang

--- luas_segitiga.py
+++ luas_segitiga.py
@@ -1,6 +1,13 @@
 #hitung luas segitiga
 ''' rumus panjang segitiga
 L = Alas * Tinggi/2'''
-alas = input("masukan alas: ")
-tinggi = input ("masukan tinggi: ")
+alas = 10
+tinggi = 12
 print ("luas segitiga :", int(alas) * int(tinggi)/2)
+
+#hitung luas persegi panjang
+"""rumus luas persegi panjang
+L = panjang * lebar """
+panjang = input("masukan panjang: ")
+lebar = input ("masukan lebar: ")
+print ("luas persegi panjang: ", int(panjang) * int(lebar)/2)
