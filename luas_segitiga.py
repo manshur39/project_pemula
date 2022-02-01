46e021cadd7fe1236bdb0a113ba41f985d43f5db
manshur39 <muhammadtm35@gmail.com>
2022-02-01 07:00:35 +0700

    add rumus luas segitiga

---
+++ luas_segitiga.py
@@ -0,0 +1,6 @@
+#hitung luas segitiga
+''' rumus panjang segitiga
+L = Alas * Tinggi/2'''
+alas = input("masukan alas: ")
+tinggi = input ("masukan tinggi: ")
+print ("luas segitiga :", int(alas) * int(tinggi)/2)
